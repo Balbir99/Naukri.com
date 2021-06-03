5f0b179563d91098fa35316e868a5a6a0b443ce3
Balbir99 <balbir.kaur@geminisolutions.in>
2021-06-03 14:26:52 +0530

    second

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EducationComponent } from './education/education.component';
+import { EmploymentComponent } from './employment/employment.component';
+import { PersonalComponent } from './personal/personal.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+
+  { path:"personal", pathMatch:'full',  component:PersonalComponent},
+  { path: "employment",pathMatch:'full', component: EmploymentComponent },
+  // {path:"", redirectTo:"/personal", pathMatch:"full"},
+  { path: "education",pathMatch:'full', component: EducationComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {ReactiveFormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { PersonalComponent } from './personal/personal.component';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { EmploymentComponent } from './employment/employment.component';
+import { RouterModule, Routes } from '@angular/router';
+import { EducationComponent } from './education/education.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+   HeaderComponent,
+    FooterComponent,
+    PersonalComponent,
+    EmploymentComponent,
+    EducationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    BsDropdownModule.forRoot(),
+    BrowserAnimationsModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
